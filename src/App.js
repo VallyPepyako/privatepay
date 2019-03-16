21d1e25ee569530e999a5ff5010dc6e16eb9867b
vally <vally.pepyako@gmail.com>
2019-03-16 19:20:21 +0300

    add react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 import { Main } from './pages/main';
+import { Blog } from './pages/blog'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <Main />
+
+      <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/blog">blog</Link>
+          </li>
+        </ul>
+
+        <Route exact path="/" component={Main} />
+        <Route path="/blog" component={Blog} />
+      </div>
+    </Router>
     );
   }
 }
---
+++ src/pages/blog.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+
+export class Blog extends PureComponent {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+            blog Learn React
+        </header>
+      </div>
+    );
+  }
+}
