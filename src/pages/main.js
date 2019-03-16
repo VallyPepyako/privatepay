2b0ac7c13ddde4c1419f873315fc9f6b66d0bb73
vally <vally.pepyako@gmail.com>
2019-03-16 19:11:05 +0300

    bottstrap cra

---
+++ src/pages/main.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+
+export class Main extends PureComponent {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+            Learn React
+        </header>
+      </div>
+    );
+  }
+}
