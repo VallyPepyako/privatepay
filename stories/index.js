4d9ff3d7012300f02c84ea1d883565eeaabc66b6
Алег <38038058+SadBamsi@users.noreply.github.com>
2019-03-18 11:19:28 +0300

    Revert "Dev"

--- stories/index.js
+++ stories/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Button } from '../src/components/button';
-import button from '../src/components/button.scss';
-import index from '../src/index.scss';
 
 storiesOf('Button', module)
   .add('with text', () => (
-    <Button className="button button--confirm" text="Подтвердить"/>
+    <Button text="ololo"/>
   ))
   
