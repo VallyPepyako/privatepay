b6b182eb4ab830ed9de4527aa77237ed03ef9f8e
vally <vally.pepyako@gmail.com>
2019-03-18 22:10:54 +0300

    add sidebar

--- src/App.js
+++ src/App.js
@@ -2,26 +2,45 @@ import React, { Component } from 'react';
 import { Main } from './pages/main';
 import { Blog } from './pages/blog'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Sidebar from "react-sidebar";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sidebarOpen: false
+    };
+    this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+  }
+  onSetSidebarOpen(open) {
+    this.setState({ sidebarOpen: open });
+  }
   render() {
     return (
+      <Sidebar
+        sidebar={<b>Sidebar content</b>}
+        open={this.state.sidebarOpen}
+        onSetOpen={this.onSetSidebarOpen}
+        pullRight={true}
+        styles={{ sidebar: { background: "white" } }}
+      >
+        <button onClick={() => this.onSetSidebarOpen(true)}>
+          Open sidebar
+        </button>
+        <Router>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/blog">blog</Link>
+            </li>
+          </ul>
 
-      <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/blog">blog</Link>
-          </li>
-        </ul>
-
-        <Route exact path="/" component={Main} />
-        <Route path="/blog" component={Blog} />
-      </div>
-    </Router>
+          <Route exact path="/" component={Main} />
+          <Route path="/blog" component={Blog} />
+      </Router>
+    </Sidebar>
     );
   }
 }
