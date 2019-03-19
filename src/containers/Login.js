b904c151a80148461b936e0e6596236c42d16368
vally <vally.pepyako@gmail.com>
2019-03-19 09:07:05 +0300

    update button in projects

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -2,18 +2,11 @@ import React, { PureComponent } from 'react'
 import { Button } from '../components/button';
 
 export default class Login extends PureComponent {
-	constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
-	handleClick() {
-		console.log("ololo")
-		// this.props.login
-	}
+
 	render() {
 		return (
 			<div>
-				 <Button title="login" modifiers={["login"]} onClick={this.props.login}/>
+				 <Button modifiers={["login"]} onClick={this.props.login}>login</Button>
 			</div>
 		)
 	}
