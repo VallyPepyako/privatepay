f0d5c8931c4f39d2d1537b3edb5678728e4ebd97
Bamsi <Maslovoleg@gmail.com>
2019-03-21 12:31:52 +0300

    fuck card with pict

--- stories/index.js
+++ stories/index.js
@@ -42,6 +42,7 @@ storiesOf('Card', module)
 	.add('card', () => (
 		<Card avatarUrl = 'https://avatarko.ru/img/kartinka/1/multfilm_gomer.png'
 					cardTitle = 'Клуб Про бизнес'
+          cardPicture = 'https://steemitimages.com/DQmXGHFUaxzaseKiMTTvbmST4T256H5Lnze12hCSmqUVAWZ/young-male-lions-playing-with-each-other-jumping-into-air-picture-id171116536.jpg'
 					cardText = 'Встреча Клуба Про бизнес с Олегом Хусаеновым 20 марта в отеле «Ренессанс Минск» пройдет закрытая встреча с Олегом Хусаеновым, сооснователем и СЕО «Зубр Капитал» и сооснователем автомобильного холдинга «Атлант-М». Это уникальная  возможность задать Олегу Ильгизов...	Еще ЕЕщ.юлюбые вопросы, касающиеся вашего бизнеса. Интерактивный формат вопросов и ответов между ментором Клуба и аудиторией. Не упустите свою возможность.'
 					timeOfActivity = '5 минут'
 					numOfMembers = '41 член'
