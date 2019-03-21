e5b89d523ef7e7a1af32ddbe3fafb8a71c07d6df
Bamsi <Maslovoleg@gmail.com>
2019-03-21 12:04:33 +0300

    fix css classes

--- src/components/card.js
+++ src/components/card.js
@@ -1,17 +1,15 @@
 import React from "react"
 import "./card.scss"
 import like, {ReactComponent as LikeIcon} from "../static/icons/like.svg"
+import menu, {ReactComponent as CardMenu} from "../static/icons/card-menu.svg"
 
 export const Card = ({avatarUrl, cardTitle, cardText, cardPicture, timeOfActivity, numOfMembers, like, dislike}) => (
 	<div className="card">
 		<button className="button card__menu-button">
-			{/*  заимпортить икноку */}
+			<CardMenu />
 		</button>
 		<div className="card__header">
-			<div className="card__logo">
-				{/*  вот тут или bg или делать абсолютом и с object-fit */}
-				<img src={avatarUrl} alt="user avatar"></img>
-			</div>
+			<div className="card__logo" style = {{ backgroundImage : `url( ${ avatarUrl })` }}></div>
 			<div className="card__info">
 				<h3 className="card__title"> { cardTitle } </h3>
 				<span className="card__blog-info">{ timeOfActivity }</span>
@@ -25,11 +23,11 @@ export const Card = ({avatarUrl, cardTitle, cardText, cardPicture, timeOfActivit
 			<button className="button voting__button">
 				<LikeIcon className="voting__icon" />
 			</button>
-			<span className="card__voting-rate">{ like }</span>
-			<button className="button voting__button">
+			<span className="card__voting-rate voting__rate">{ like }</span>
+			<button className="button voting__button voting__button--180deg">
 				<LikeIcon className="voting__icon voting__icon--180deg" />
 			</button>
-			<span className="voting__rate">{ dislike }</span>
+			<span className="card__voting-rate voting__rate">{ dislike }</span>
 		</div>
 	</div>
 )
