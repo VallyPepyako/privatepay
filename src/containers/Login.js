3a80be98343544a642d597a67b039320f9263e05
Polina Kozich <polina.kozich@pandadoc.com>
2019-03-24 00:11:42 +0300

    add logo, finish first screen

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,32 +1,32 @@
 import React, { PureComponent } from 'react'
 import { Button } from '../components/button';
 import { Input } from "../components/input";
+import './login.scss';
+// eslint-disable-next-line
+import logo, {ReactComponent as Logo} from '../static/liberty-logo.svg'
 
 export default class Login extends PureComponent {
 
 	render() {
 		return (
-			<div className="login-block">
-				<form className="form-login">
-					<div className="form-login__input">
-						<Input type="e-mail"
-							   placeholder="e-mail"
-							   modifiers="login"
-						/>
-					</div>
-                    <div className="form-login__input">
-						<Input type="password"
-							   placeholder="password"
-							   modifiers="login"
-						/>
-					</div>
-					<div className="form-login__submit">
-						<Button modifiers="login" onClick={this.props.login}>login</Button>
-					</div>
+			<section className="login">
+				<form className="login__form">
+					<Logo className="logo login__logo"/>
+					<Input type="e-mail"
+						   placeholder="e-mail"
+						   modifiers="login login__input"
+					/>
+					<Input type="password"
+						   placeholder="password"
+						   modifiers="login login__input"
+					/>
+					<Button modifiers="login login__submit" onClick={this.props.login}>войти</Button>
 				</form>
-
-				<Button modifiers="link" onClick={this.props.showRegestraion}>Registration</Button>
-			</div>
+				<footer className="login__footer">
+					<span>Нет аккаунта?&nbsp;</span>
+					<Button modifiers="link color-white" onClick={this.props.showRegestraion}>Регистрация</Button>
+				</footer>
+			</section>
 		)
 	}
 }
