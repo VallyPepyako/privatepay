e63d6666d2417687f6f24e3c7d0dc194c13068bf
Polina Kozich <polina.kozich@pandadoc.com>
2019-03-23 13:56:58 +0300

    login init

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,13 +1,31 @@
 import React, { PureComponent } from 'react'
 import { Button } from '../components/button';
+import { Input } from "../components/input";
 
 export default class Login extends PureComponent {
 
 	render() {
 		return (
-			<div>
-				 <Button modifiers="login" onClick={this.props.login}>login</Button>
-				 <Button modifiers="link" onClick={this.props.showRegestraion}>regestration</Button>
+			<div className="login-block">
+				<form className="form-login">
+					<div className="form-login__input">
+						<Input type="e-mail"
+							   placeholder="e-mail"
+							   modifiers="login"
+						/>
+					</div>
+                    <div className="form-login__input">
+						<Input type="password"
+							   placeholder="password"
+							   modifiers="login"
+						/>
+					</div>
+					<div className="form-login__submit">
+						<Button modifiers="login" onClick={this.props.login}>login</Button>
+					</div>
+				</form>
+
+				<Button modifiers="link" onClick={this.props.showRegestraion}>Registration</Button>
 			</div>
 		)
 	}
